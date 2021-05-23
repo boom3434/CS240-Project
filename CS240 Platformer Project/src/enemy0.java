577df9803572f30d477fc57056ccc87114fb60d9
ThisIsBryceJ <83939099+ThisIsBryceJ@users.noreply.github.com>
2021-05-23 00:48:07 -0700

    Added more complex movement
    Figured it was worth working on the base enemies to determine a way to make their moves a little more dynamic; this should serve as a good template for other enemies.

--- CS240 Platformer Project/src/enemy0.java
+++ CS240 Platformer Project/src/enemy0.java
@@ -1,17 +1,23 @@
 import java.awt.Graphics;
 
 public class enemy0 extends Creature {
+	
+	private int step;
+	
 	public enemy0(Handler handler, float x, float y, int width, int height) {
 		super(handler, x, y, width, height);
 		bounds.x = 0;
 		bounds.y = 0;
 		bounds.width = width;
 		bounds.height = height;
+		
+		step = 0;
 	}
 
 	public void tick() {
 		getInput();
 		move();
+		step++;
 	}
 
 	public void die() {
@@ -21,10 +27,29 @@ public class enemy0 extends Creature {
 	private void getInput() {
 		xMove = 0;
 		yMove = 0;
+
+		int movementScale = 30;
 		
-			yMove = -speed;
-		if (this.checkEntityCollisions(0, 10))
+		if(step < movementScale) {
 			yMove = speed;
+		} else if (step >= movementScale && step < movementScale*2) {
+			yMove = 0;
+		} else if (step >= movementScale*2 && step < movementScale*3) {
+			xMove = speed;
+		} else if (step >= movementScale*3 && step < movementScale*4) {
+			xMove = 0;
+		} else if (step >= movementScale*4 && step < movementScale*5) {
+			yMove = -speed;
+		} else if (step >= movementScale*5 && step < movementScale*6) {
+			yMove = 0;
+		} else if (step >= movementScale*6 && step < movementScale*7) {
+			xMove = -speed;
+		} else if (step >= movementScale*7 && step < movementScale*8) {
+			xMove = 0;
+		}
+		if (step == movementScale*8) {
+			step = 0;
+		}
 		
 	}
 
