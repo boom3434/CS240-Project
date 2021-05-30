cc5409f3c1a6f2142c3927586745e3d9b1cc37f5
boom3434 <finn.parker3434@gmail.com>
2021-05-30 15:10:22 -0700

    Made a working enemy1

--- CS240 Platformer Project/src/dev/platformer/entities/creatures/enemy1.java
+++ CS240 Platformer Project/src/dev/platformer/entities/creatures/enemy1.java
@@ -1,7 +1,11 @@
 package platformer.entities.creatures;
+
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
 
 import platformer.Handler;
 import platformer.entities.Entity;
@@ -24,13 +28,13 @@ public class enemy1 extends Creature {
 	// Collision Rectangles
 
 	public enemy1(Handler handler, float x, float y, int width, int height) {
-		
+
 		super(handler, x, y, width, height);
 		bounds.x = 0;
 		bounds.y = 0;
 		bounds.width = width;
 		bounds.height = height;
-	
+
 		step = 0;
 
 		// Animations
@@ -50,8 +54,8 @@ public class enemy1 extends Creature {
 	public void die() {
 
 	}
-	
-	//TODO: Fix enemy attack rates
+
+	// TODO: Fix enemy attack rates
 
 	private void checkAttacks() {
 		attackTimer += System.currentTimeMillis() - lastAttackTimer;
@@ -90,52 +94,158 @@ public class enemy1 extends Creature {
 		arR.x = cb.x + cb.width;
 		arR.y = cb.y + cb.height / 2 - arSize / 2;
 
-		
-		
 		for (Entity e : handler.getWorld().getEntityManager().getEntities()) {
 			if (e.equals(this)) {
 				continue;
 			}
-			if ( ( e.getCollisionBounds(0, 0).intersects(arU)  || e.getCollisionBounds(0, 0).intersects(arD) 
-					|| e.getCollisionBounds(0, 0).intersects(arL)  || e.getCollisionBounds(0, 0).intersects(arR) ) && attackTimer>attackCooldown ) {
+			if ((e.getCollisionBounds(0, 0).intersects(arU) || e.getCollisionBounds(0, 0).intersects(arD)
+					|| e.getCollisionBounds(0, 0).intersects(arL) || e.getCollisionBounds(0, 0).intersects(arR))
+					&& attackTimer > attackCooldown) {
 				attackTimer = 0;
 				e.hurt(1);
-				
+
 				return;
 			} else {
 				return;
 			}
 		}
-		//attackTimer = 0;
+		// attackTimer = 0;
 	}
 
 	private void getInput() {
 		xMove = 0;
 		yMove = 0;
-
+		int moveSelect = 1;
+		if (step > 50)
+			moveSelect = getMovePattern();
+		// changed all speeds to 5.0f
 		int movementScale = 30;
-
-		if (step < movementScale) {
-			yMove = speed;
-		} else if (step >= movementScale && step < movementScale * 2) {
-			yMove = 0;
-		} else if (step >= movementScale * 2 && step < movementScale * 3) {
-			xMove = speed;
-		} else if (step >= movementScale * 3 && step < movementScale * 4) {
-			xMove = 0;
-		} else if (step >= movementScale * 4 && step < movementScale * 5) {
-			yMove = -speed;
-		} else if (step >= movementScale * 5 && step < movementScale * 6) {
-			yMove = 0;
-		} else if (step >= movementScale * 6 && step < movementScale * 7) {
-			xMove = -speed;
-		} else if (step >= movementScale * 7 && step < movementScale * 8) {
-			xMove = 0;
+		if (moveSelect == 0) {
+			if (step < movementScale) {
+				yMove = 5.0f;
+			} else if (step >= movementScale && step < movementScale * 2) {
+				yMove = 0;
+			} else if (step >= movementScale * 2 && step < movementScale * 3) {
+				xMove = 5.0f;
+			} else if (step >= movementScale * 3 && step < movementScale * 4) {
+				xMove = 0;
+			} else if (step >= movementScale * 4 && step < movementScale * 5) {
+				yMove = -5.0f;
+			} else if (step >= movementScale * 5 && step < movementScale * 6) {
+				yMove = 0;
+			} else if (step >= movementScale * 6 && step < movementScale * 7) {
+				xMove = -5.0f;
+			} else if (step >= movementScale * 7 && step < movementScale * 8) {
+				xMove = 0;
+			}
+			if (step == movementScale * 8) {
+				step = 0;
+				moveSelect = getMovePattern();
+			}
+		} else if (moveSelect == 1) {
+			if (step < movementScale) {
+				yMove = -1.0f;
+			} else if (step >= movementScale && step < movementScale * 2) {
+				yMove = 0;
+			} else if (step >= movementScale * 2 && step < movementScale * 3) {
+				xMove = -1.0f;
+			} else if (step >= movementScale * 3 && step < movementScale * 4) {
+				xMove = 0;
+			} else if (step >= movementScale * 4 && step < movementScale * 5) {
+				yMove = 1.0f;
+			} else if (step >= movementScale * 5 && step < movementScale * 6) {
+				yMove = 0;
+			} else if (step >= movementScale * 6 && step < movementScale * 7) {
+				xMove = 1.0f;
+			} else if (step >= movementScale * 7 && step < movementScale * 8) {
+				xMove = 0;
+			}
+			if (step == movementScale * 8) {
+				step = 0;
+				moveSelect = getMovePattern();
+			}
+		} else if (moveSelect == 2) {
+			if (step < movementScale) {
+				xMove = 0;
+			} else if (step >= movementScale && step < movementScale * 2) {
+				xMove = -5.0f;
+			} else if (step >= movementScale * 2 && step < movementScale * 3) {
+				yMove = 0;
+			} else if (step >= movementScale * 3 && step < movementScale * 4) {
+				yMove = -5.0f;
+			} else if (step >= movementScale * 4 && step < movementScale * 5) {
+				xMove = 0;
+			} else if (step >= movementScale * 5 && step < movementScale * 6) {
+				xMove = 5.0f;
+			} else if (step >= movementScale * 6 && step < movementScale * 7) {
+				yMove = 5.0f;
+			} else if (step >= movementScale * 7 && step < movementScale * 8) {
+				yMove = 0;
+			}
+			if (step == movementScale * 8) {
+				step = 0;
+				moveSelect = getMovePattern();
+			}
+		} else if (moveSelect == 3) {
+			if (step < movementScale) {
+				yMove = -10.0f;
+			} else if (step >= movementScale && step < movementScale * 2) {
+				yMove = 0;
+			} else if (step >= movementScale * 2 && step < movementScale * 3) {
+				xMove = -10.0f;
+			} else if (step >= movementScale * 3 && step < movementScale * 4) {
+				xMove = 0;
+			} else if (step >= movementScale * 4 && step < movementScale * 5) {
+				yMove = 10.0f;
+			} else if (step >= movementScale * 5 && step < movementScale * 6) {
+				yMove = 0;
+			} else if (step >= movementScale * 6 && step < movementScale * 7) {
+				xMove = 10.0f;
+			} else if (step >= movementScale * 7 && step < movementScale * 8) {
+				xMove = 0;
+			}
+			if (step == movementScale * 8) {
+				step = 0;
+				moveSelect = getMovePattern();
+			}
+		} else {
+			// TODO: make changes to this else statement to get different movement patterns
+			if (step < movementScale) {
+				yMove = speed;
+			} else if (step >= movementScale && step < movementScale * 2) {
+				yMove = 0;
+			} else if (step >= movementScale * 2 && step < movementScale * 3) {
+				xMove = speed;
+			} else if (step >= movementScale * 3 && step < movementScale * 4) {
+				xMove = 0;
+			} else if (step >= movementScale * 4 && step < movementScale * 5) {
+				yMove = -speed;
+			} else if (step >= movementScale * 5 && step < movementScale * 6) {
+				yMove = 0;
+			} else if (step >= movementScale * 6 && step < movementScale * 7) {
+				xMove = -speed;
+			} else if (step >= movementScale * 7 && step < movementScale * 8) {
+				xMove = 0;
+			}
+			if (step == movementScale * 8) {
+				step = 0;
+				moveSelect = getMovePattern();
+			}
 		}
-		if (step == movementScale * 8) {
-			step = 0;
+
+	}
+
+	public int getMovePattern() {
+		Queue<Integer> q = new LinkedList<>();
+
+		for (int i = 0; i < 4; i++) {
+			Random rand = new Random();
+			int upperbound = 5;
+			int insert = rand.nextInt(upperbound);
+			q.add(insert);
 		}
 
+		return q.remove();
 	}
 
 	public void render(Graphics g) {
--- CS240 Platformer Project/src/dev/platformer/worlds/World.java
+++ CS240 Platformer Project/src/dev/platformer/worlds/World.java
@@ -1,10 +1,12 @@
 package platformer.worlds;
+
 import java.awt.Graphics;
 
 import platformer.Handler;
 import platformer.entities.EntityManager;
 import platformer.entities.creatures.Hero;
 import platformer.entities.creatures.enemy0;
+import platformer.entities.creatures.enemy1;
 import platformer.entities.statics.Rock;
 import platformer.entities.statics.Tree;
 import platformer.tiles.Tile;
@@ -16,89 +18,85 @@ public class World {
 	private int width, height;
 	private int spawnX, spawnY;
 	private int[][] tiles;
-	//Entities
+	// Entities
 	private EntityManager entityManager;
-	
-	public World(Handler handler, String path){
+
+	public World(Handler handler, String path) {
 		this.handler = handler;
-		entityManager = new EntityManager(handler, new Hero(handler, 200, 200));
+		entityManager = new EntityManager(handler, new Hero(handler, 10, 10));
 		// Temporary entity code!
 		entityManager.addEntity(new Tree(handler, 100, 250));
 		entityManager.addEntity(new Rock(handler, 100, 450));
-	
-		
+
 		loadWorld(path);
-		
+
 		entityManager.getPlayer().setX(spawnX);
 		entityManager.getPlayer().setY(spawnY);
 		entityManager.addEntity(new enemy0(handler, 400, 300, 50, 50));
-		entityManager.addEntity(new enemy0(handler,500,350,50,50));
+		entityManager.addEntity(new enemy0(handler, 500, 350, 50, 50));
+		entityManager.addEntity(new enemy1(handler, 700, 700, 50, 50));
+		entityManager.addEntity(new enemy1(handler,500,500,50,50));
+		entityManager.addEntity(new enemy1(handler,650,500,50,50));
 	}
-	
-	public void tick(){
+
+	public void tick() {
 		entityManager.tick();
 	}
-	
-	public void render(Graphics g){
+
+	public void render(Graphics g) {
 		int xStart = (int) Math.max(0, handler.getGameCamera().getxOffset() / Tile.TILEWIDTH);
-		int xEnd = (int) Math.min(width, (handler.getGameCamera().getxOffset() + handler.getWidth()) / Tile.TILEWIDTH + 1);
+		int xEnd = (int) Math.min(width,
+				(handler.getGameCamera().getxOffset() + handler.getWidth()) / Tile.TILEWIDTH + 1);
 		int yStart = (int) Math.max(0, handler.getGameCamera().getyOffset() / Tile.TILEHEIGHT);
-		int yEnd = (int) Math.min(height, (handler.getGameCamera().getyOffset() + handler.getHeight()) / Tile.TILEHEIGHT + 1);
-		
-		for(int y = yStart;y < yEnd;y++){
-			for(int x = xStart;x < xEnd;x++){
+		int yEnd = (int) Math.min(height,
+				(handler.getGameCamera().getyOffset() + handler.getHeight()) / Tile.TILEHEIGHT + 1);
+
+		for (int y = yStart; y < yEnd; y++) {
+			for (int x = xStart; x < xEnd; x++) {
 				getTile(x, y).render(g, (int) (x * Tile.TILEWIDTH - handler.getGameCamera().getxOffset()),
 						(int) (y * Tile.TILEHEIGHT - handler.getGameCamera().getyOffset()));
 			}
 		}
-		//Entities
+		// Entities
 		entityManager.render(g);
 	}
-	
-	public Tile getTile(int x, int y){
-		if(x < 0 || y < 0 || x >= width || y >= height)
+
+	public Tile getTile(int x, int y) {
+		if (x < 0 || y < 0 || x >= width || y >= height)
 			return Tile.grassTile;
-		
+
 		Tile t = Tile.tiles[tiles[x][y]];
-		if(t == null)
+		if (t == null)
 			return Tile.dirtTile;
 		return t;
 	}
-	
-	private void loadWorld(String path){
+
+	private void loadWorld(String path) {
 		String file = Utils.loadFileAsString(path);
 		String[] tokens = file.split("\\s+");
 		width = Utils.parseInt(tokens[0]);
 		height = Utils.parseInt(tokens[1]);
 		spawnX = Utils.parseInt(tokens[2]);
 		spawnY = Utils.parseInt(tokens[3]);
-		
+
 		tiles = new int[width][height];
-		for(int y = 0;y < height;y++){
-			for(int x = 0;x < width;x++){
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
 				tiles[x][y] = Utils.parseInt(tokens[(x + y * width) + 4]);
 			}
 		}
 	}
-	
-	public int getWidth(){
+
+	public int getWidth() {
 		return width;
 	}
-	
-	public int getHeight(){
+
+	public int getHeight() {
 		return height;
 	}
 
 	public EntityManager getEntityManager() {
 		return entityManager;
 	}
-	
+
 }
-
-
-
-
-
-
-
-
